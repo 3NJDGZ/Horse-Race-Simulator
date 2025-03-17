0127d49c09275546558b908e159ccf6c36bd5b63
Alejandro.DGZ <3NJDEGUZMAN@gmail.com>
2025-03-17 21:15:57 +0000

    finished horse class, setup race class, now looking...

--- part1/Main.java
+++ part1/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        Horse testHorseOne = new Horse("Roach", '1', 0.5);
+        // Horse testHorseTwo = new Horse("Leya", '1', 10);
+        System.out.println("Horse Name: " + testHorseOne.getName());
 
     }
 }
