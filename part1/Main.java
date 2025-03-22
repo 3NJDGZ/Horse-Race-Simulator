b8d5ec86e030443b4de5c9d3e44212720b578082
Alejandro.DGZ <3NJDEGUZMAN@gmail.com>
2025-03-22 15:47:23 +0000

    finished testing the constructor of the horse class

--- part1/Main.java
+++ part1/Main.java
@@ -1,12 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        Horse horse1 = new Horse("Roach", '1', 0.5);
-        Horse horse2 = new Horse("Leya", '2', 0.5);
-        Horse horse3 = new Horse("Gilbert", '3', 0.5);
-        Race race = new Race(10);
-        race.addHorse(horse1, 1);
-        race.addHorse(horse2, 2);
-        race.addHorse(horse3, 3);
-        race.startRace();
+        // Horse horse1 = new Horse("Roach", '1', 0.5);
+        // Horse horse2 = new Horse("Leya", '2', 0.5);
+        // Horse horse3 = new Horse("Gilbert", '3', 0.5);
+        // Race race = new Race(10);
+        // race.addHorse(horse1, 1);
+        // race.addHorse(horse2, 2);
+        // race.addHorse(horse3, 3);
+        // race.startRace();
+
+        // test constructor
+        try {
+            Horse testHorse = new Horse("Roach", 'R', 0);
+            System.out.println("No Bug!");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
     }
 }
